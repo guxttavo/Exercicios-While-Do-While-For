7935c1d5b09073e1668fc6374a76fcc30fbbfdae
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-20 17:03:48 -0300

    Ex 04

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,15 +1,29 @@
 /*
-
-
+-->Supondo que a população de um país A seja da ordem de 80.000 habitantes com uma taxa anual de crescimento de
+   3% e que a população de B, seja 200.000 habitantes com uma taxa de crescimento de 1.5%.
+-->Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse
+   ou iguale a população do país B, mantidas as taxas de crescimento.
  */
-
-import java.util.Scanner;
 
 public class Ex04 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        double popA = 80.000;
+        double popB = 200.000;
+        int cont = 0;
+
+        while (popA < popB) {
+
+            popA += (popA / 100) * 3;
+            popB += (popA / 100) * 1.5;
+            cont++;
+        }
+
+        System.out.println("População A: " + popA);
+        System.out.println("População B: " + popB);
+        System.out.println("Anos: " + cont);
+
 
     }
 }
