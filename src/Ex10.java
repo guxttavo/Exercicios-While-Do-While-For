8cfd102e67a47fe2270c2cb5ad89464c796b0bb0
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-24 18:21:40 -0300

    Ex 10

--- src/Ex10.java
+++ src/Ex10.java
@@ -1,6 +1,6 @@
 /*
-
-
+-->Faça um programa que receba dois números inteiros e gere os números inteiros que estão
+   no intervalo compreendido por eles.
  */
 
 import java.util.Scanner;
@@ -10,6 +10,18 @@ public class Ex10 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if (num1 < num2) {
+            for (int i = num1; i <= num2; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("O primeiro número precisa ser maior que o segundo!");
+        }
 
     }
 }
