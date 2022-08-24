267608912052aec679071c265d7626fc113b3ce3
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-24 18:35:08 -0300

    Ex 11

--- src/Ex11.java
+++ src/Ex11.java
@@ -1,7 +1,6 @@
 /*
-
-
- */
+-->Altere o programa anterior para mostrar no final a soma dos números.
+*/
 
 import java.util.Scanner;
 
@@ -10,6 +9,19 @@ public class Ex11 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int soma = 0;
+        if (num1 < num2) {
+            for (int i = num1; i <= num2; i++) {
+                soma += i;
+            }
+        }
+
+        System.out.println("Soma dos números: " + soma);
 
     }
 }
