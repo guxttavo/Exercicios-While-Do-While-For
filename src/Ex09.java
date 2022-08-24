ca3bf58a3776d114a2003949792efeaeba020fc1
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-24 18:10:40 -0300

    Ex 09

--- src/Ex09.java
+++ src/Ex09.java
@@ -1,7 +1,6 @@
 /*
-
-
- */
+-->Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+*/
 
 import java.util.Scanner;
 
@@ -9,7 +8,12 @@ public class Ex09 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
 
     }
 }
