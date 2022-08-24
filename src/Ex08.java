59cfe49f2d63bb73de29d09be2a50aba6dedbbc2
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-24 18:06:40 -0300

    Ex 08

--- src/Ex08.java
+++ src/Ex08.java
@@ -1,7 +1,6 @@
 /*
-
-
- */
+-->Faça um programa que leia 5 números e informe a soma e a média dos números.
+*/
 
 import java.util.Scanner;
 
@@ -10,6 +9,22 @@ public class Ex08 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        int num;
+        int soma = 0;
+        double media;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite 5 números: ");
+            num = sc.nextInt();
+
+            soma += num;
+        }
+
+        media = soma / 5;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
 
     }
 }
